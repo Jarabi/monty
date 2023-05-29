a3732ebfa463d6c4ca8e802da6d4a9348d4264f7
Alex Jarabi <jarabialex.gmail.com>
2023-05-28 18:25:19 -0700

    Implemented push and pall opcodes.

--- error_handling.c
+++ error_handling.c
@@ -1,69 +1,36 @@
 #include "monty.h"
 
 /**
- * _err - Handles error status
+ * _err - Error handling
  * @status: Error status
  */
 void _err(int status, ...)
 {
 	va_list args;
-	char *msg;
-	size_t msg_size = 100;
+	int line_num;
+	char *opcode;
 
 	va_start(args, status);
-	msg = malloc(msg_size);
-
-	if (msg == NULL)
-	{
-		_write("Error: malloc failed");
-		exit(EXIT_FAILURE);
-	}
-
-	print_err(status, args, msg, msg_size);
-	free(msg);
-	exit(EXIT_FAILURE);
-}
-
-/**
- * print_err - Prints error message to stdout
- * @status: Error status
- * @args: Error status arguments
- * @msg: Error message
- * @msg_size: Error message size
- */
-void print_err(int status, va_list args, char *msg, size_t msg_size)
-{
-	char line_num_str[20];
-
 	switch (status)
 	{
 		case 0:
-			strncpy(msg, "USAGE: monty file", msg_size);
+			fprintf(stderr, "USAGE: monty file\n");
 			break;
 		case 1:
-			strncpy(msg, "Error: Can't open file ", msg_size);
-			strncat(msg, va_arg(args, char *), msg_size - strlen(msg) - 1);
+			fprintf(stderr, "USAGE: monty file %s\n", va_arg(args, char *));
 			break;
 		case 2:
-			/* strncat expects a string, not an int. Converting */
-			/* line number <int> to string representation. */
-			sprintf(line_num_str, "%d", va_arg(args, int));
-			strncpy(msg, "L", msg_size);
-			strncat(msg, line_num_str, msg_size - strlen(msg) - 1);
-			strcat(msg, ": unknown instruction ");
-			strncat(msg, va_arg(args, char *), msg_size - strlen(msg) - 1);
+			line_num = va_arg(args, int);
+			opcode = va_arg(args, char*);
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_num, opcode);
 			break;
 		case 3:
-			sprintf(line_num_str, "%d", va_arg(args, int));
-			strncpy(msg, "L", msg_size);
-			strncat(msg, line_num_str, msg_size - strlen(msg) - 1);
-			strcat(msg, ": usage: push integer");
+			fprintf(stderr, "L%d: usage: push integer\n", va_arg(args, int));
 			break;
 		case 4:
-			strncpy(msg, "Error: malloc failed", msg_size);
+			fprintf(stderr, "Error: malloc failed\n");
 			break;
 	}
 	va_end(args);
-	_write(msg);
-	putchar('\n');
+	exit(EXIT_FAILURE);
 }
